a041a8c7f39e4093d240c7aa79f832ab3b1df819
CasperFicke <cficke@quicknet.nl>
2020-09-15 12:25:09 +0200

    add bevraging iexcloud api

--- quotes/views.py
+++ quotes/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+import os
 
-# Create your views here.
+# home view.
 def home(request):
-  return render(request, 'home.html', {})
+  import json
+  import requests
+  iexcloud_apikey = os.getenv('IEXCLOUD_APIKEY')
 
-
-# Create your views here.
+  if request.method == "POST":
+    ticker = request.POST['ticker_symbol']
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/"+ ticker + "/quote?token=" + iexcloud_apikey)
+    try:
+      api_result = json.loads(api_request.content)
+    except Exception as e:
+      api_result = "Error..."
+    return render(request, 'home.html', {'api_result': api_result})
+  else:
+    return render(request, 'home.html', {'ticker': "enter a ticker symbol above.."})
+  
+# about view
 def about(request):
   return render(request, 'about.html', {})
