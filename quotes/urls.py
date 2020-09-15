090c39e738aaac846a555224d99a9c9347fed523
CasperFicke <cficke@quicknet.nl>
2020-09-15 22:26:38 +0200

    add ticker dictionary

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-     path('about/', views.about, name='about'),
+    path('about/', views.about, name='about'),
+    path('add_stock/', views.add_stock, name='add_stock'),
+    path('delete_stock/<stock_id>', views.delete_stock, name='delete_stock'),
+    path('delete_stocks/', views.delete_stocks, name='delete_stocks'),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import StockForm
+
 import os
 
-# home view.
+from .models import Stock
+
+# home view
 def home(request):
   import json
   import requests
@@ -21,3 +26,43 @@ def home(request):
 # about view
 def about(request):
   return render(request, 'about.html', {})
+
+# add stock view
+def add_stock(request):
+  import json
+  import requests
+  iexcloud_apikey = os.getenv('IEXCLOUD_APIKEY')
+  if request.method == "POST":
+    ticker = request.POST['ticker']
+    add_stock_form = StockForm(request.POST or None)
+
+    if add_stock_form.is_valid():
+      add_stock_form.save()
+      messages.success(request, ("Stock has been added"))
+      return redirect('add_stock')
+
+  else:  
+    ticker = Stock.objects.all()
+    output = []
+    for ticker_item in ticker:
+      api_request = requests.get("https://cloud.iexapis.com/stable/stock/"+ str(ticker_item) + "/quote?token=" + iexcloud_apikey)
+      try:
+        api_result = json.loads(api_request.content)
+        output.append(api_result)
+      except Exception as e:
+        api_result = "Error..."
+
+    return render(request, 'add_stock.html', {'ticker': ticker, 'output': output})
+
+# delete stock view
+def delete_stock(request, stock_id):
+  item = Stock.objects.get(pk=stock_id)
+  item.delete()
+  messages.success(request, ("Stock has been deleted!"))
+  return redirect(delete_stocks)
+
+
+# delete stocks view
+def delete_stocks(request):
+  ticker = Stock.objects.all()
+  return render(request, 'delete_stocks.html', {'ticker': ticker})
